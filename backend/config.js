2b44291b2cfcf49ff794b567c521c188032120bc
yzeman <127164094+yzeman@users.noreply.github.com>
2025-09-28 14:20:16 +0100

    Create config.js

---
+++ backend/config.js
@@ -0,0 +1,10 @@
+// backend/config.js
+const config = {
+  PORT: 5000,
+  MONGODB_URI: 'mongodb+srv://olayinka2222isreal:YOUR_NEW_PASSWORD@yzemanbot.weaom9s.mongodb.net/giftcard_marketplace?retryWrites=true&w=majority',
+  JWT_SECRET: 'gc_marketplace_prod_2024_NEW_SECRET_CHANGE_THIS',
+  TELEGRAM_BOT_TOKEN: '6235048166:AAE7jQItOA3n5tqn_971ih6RQ8qvPY4V7X0',
+  TELEGRAM_CHAT_ID: '1828689837'
+};
+
+module.exports = config;
